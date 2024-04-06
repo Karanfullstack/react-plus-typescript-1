27482d8d79b58461023cfced27132c1dbfeabea1
Karan Chauhan <karanchouhan211@gmail.com>
2024-04-06 10:44:21 +0200

    passing children props

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-	const items = [
-		"An item",
-		"A second item",
-		"A third item",
-		"A fourth item",
-		"And a fifth one",
-	];
-const onSelectItem	= (item: string) => console.log(item);
 	return (
 		<div className="App">
-			<ListGroup items={items} heading="Countries" onSelectItem={onSelectItem} />
+			<Alert>
+				hello <em>world</em>
+			</Alert>
 		</div>
 	);
 }
---
+++ src/components/Alert.tsx
@@ -0,0 +1,8 @@
+import React, { ReactNode } from "react";
+
+interface AlertProps {
+	children: ReactNode;
+}
+export default function Alert({ children }: AlertProps) {
+	return <div className="alert alert-primary">{children}</div>;
+}
