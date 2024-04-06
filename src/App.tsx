a09107b9bc26b8a843890b9a93f384d865732e54
Karan Chauhan <karanchouhan211@gmail.com>
2024-04-06 21:12:27 +0200

    updating objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import Like from "./Like";
-
+import { useState } from "react";
 function App() {
+	const [isBug, setBug] = useState([
+		{ id: 1, title: "bug1", fixed: false },
+		{ id: 2, title: "bug2", fixed: false },
+	]);
+
+	const handelDrink = () => {
+		setBug(isBug.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+	};
 	return (
 		<div className="App">
-			<Like onClick={() => console.log("clicked")} />
+			{isBug.map((bug) => (
+				<div key={bug.id}>
+					<h3>{bug.title}</h3>
+					<p>{bug.fixed ? "Fixed" : "Not Fixed"}</p>
+				</div>
+			))}
+			<button onClick={handelDrink}>Button</button>
 		</div>
 	);
 }
