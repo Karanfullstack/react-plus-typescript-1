7428f7d8ef036c6b8ed7a5b26adfd611bd731a6d
Karan Chauhan <karanchouhan211@gmail.com>
2024-04-09 15:01:08 +0200

    Expanse tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import Form from "./components/Form/Form";
+import { useState } from "react";
+import ExpanseForm from "./components/ExpanseForm/ExpanseForm";
+import Table from "./Table/Table";
 
+export interface DataProps {
+	readonly id: string;
+	description: string;
+	amount: number;
+	category: string;
+}
 function App() {
+	const [data, setData] = useState<DataProps[]>([]);
+
+	const addData = (data: DataProps) => {
+		const Data: DataProps = {
+			id: Date.now().toString(),
+			description: data.description,
+			amount: data.amount,
+			category: data.category,
+		};
+		setData((prev) => [...prev, Data]);
+	};
+
+	const onDelete = (id: string) => {
+		const newData = data.filter((item) => item.id !== id);
+		setData(newData);
+	};
 	return (
 		<div>
-			<Form />
+			<ExpanseForm addData={addData} />
+			<Table data={data} onDelete={onDelete} />
 		</div>
 	);
 }
