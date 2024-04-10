ba55b2e73e4baa92def33f251446946b5181994e
Karan Chauhan <karanchouhan211@gmail.com>
2024-04-10 17:41:45 +0200

    loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,22 @@ interface IUser {
 function App() {
 	const [users, setUsers] = useState<IUser[]>([]);
 	const [error, setError] = useState<string | null>(null);
-
+	const [loading, setLoading] = useState<boolean>(false);
 	useEffect(() => {
 		const controller = new AbortController();
+		setLoading(true);
 		axios
 			.get<IUser[]>("https://jsonplaceholder.typicode.com/users", {
 				signal: controller.signal,
 			})
-			.then((res) => setUsers(res.data))
+			.then((res) => {
+				setUsers(res.data);
+				setLoading(false);
+			})
 			.catch((err: AxiosError) => {
 				if (err instanceof CanceledError) return;
 				setError(err.message);
+				setLoading(false);
 			});
 		return () => controller.abort();
 	}, []);
@@ -29,6 +34,7 @@ function App() {
 				<li key={user.id}>{user.name}</li>
 			))}
 			{error && <div className="text-danger">{error}</div>}
+			{loading && <div className="spinner-border"></div>}
 		</div>
 	);
 }
